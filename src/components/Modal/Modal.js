ff7605615d9468f22e24ed5a53628251787d3f8b
Daniel Kim <kim.daniel24@gmail.com>
2018-09-29 10:11:10 -0400

    change

--- src/components/Modal/Modal.js
+++ src/components/Modal/Modal.js
@@ -19,7 +19,7 @@ function Modal() {
 			<div className="modal-footer white-text center">
 				<a className="waves-effect waves-light btn" onClick={() => {
 					document.getElementsByClassName('instructionsModal')[0].classList.add('hide');
-				}}>Go Get Em!</a>
+				}}>Don't Forget!</a>
 			</div>	
 		</div>
 	)
